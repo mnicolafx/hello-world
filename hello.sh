1e109a31cbc6f0b397e1e25e83334282067d20ac
Matteo Nicola <Matteo.Nicola@FOXEL.com>
2021-05-19 14:00:45 +0200

    modified hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "Hello World"
+# Usage: Hello World Bash Shell Script Using Variables
+# Author: Matteo Nicola
+# -------------------------------------------------
+ 
+# Define bash shell variable called var 
+# Avoid spaces around the assignment operator (=)
+var="Hello World"
+ 
+# print it 
+echo "$var"
+ 
+# Another way of printing it
+printf "%s\n" "$var"
